5aabb5a4a4733bf433464bd3bcf3155ed6903c72
rodgaravi <rodrigogarciaavila26@gmail.com>
2021-07-25 17:08:52 -0500

    Lumen passport install and app.php was configurated

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -98,9 +98,11 @@ $app->configure('app');
 |
 */
 
-// $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+//$app->register(App\Providers\AppServiceProvider::class);
+$app->register(App\Providers\AuthServiceProvider::class);
+$app->register(Laravel\Passport\PassportServiceProvider::class);
+$app->register(Dusterio\LumenPassport\Passport\PassportServiceProvider::class);
 $app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 
 /*
