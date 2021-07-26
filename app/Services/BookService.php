2fd0822563e580ef0508650474d84511db2719d3
rodgaravi <rodrigogarciaavila26@gmail.com>
2021-07-25 19:10:57 -0500

    Secret was added like a header in Triats and like a attributte on services

--- app/Services/BookService.php
+++ app/Services/BookService.php
@@ -14,9 +14,16 @@ class BookService
      */
     public $baseUri;
 
+    /**
+     * the  secret to be used to consume the books' service
+     * @var string
+     */
+    public $secret;
+
     public function __construct()
     {
         $this->baseUri = config('services.books.base_uri');
+        $this->secret = config('services.books.secret');
     }
 
      /**
